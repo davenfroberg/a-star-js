bea454de0528a122609f66656b5488bb3d9eeb37
davenfroberg <davenfroberg@gmail.com>
2022-08-14 00:50:25 -0600

    Started working on map generation

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function Node(position, wall, target, xpos, ypos) {
 let dim = 30;
 let targPos, startPos;
 let wallProbability = 30; // in percent
+let map = [];
+
+function createRow() {
+    let row = [];
+    for (let i = 0; i < dim; i++) {
+        row[i] = new Node();
+    }
+    return row;
+}
+
+function assignWalls() {
+    for (let i = 0; i < dim; i++) {
+        for (let j = 0; j < dim; j++) {
+            if (Math.floor(Math.random() * 100) < wallProbability) {
+                map[i][j].wall = true;
+            }
+        }
+    }
+}
+
+function createMap() {
+    for (let i = 0; i < dim; i++) {
+        map[i] = createRow();
+    }
+    assignWalls();
+}
+
+
