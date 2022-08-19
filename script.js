4be0fec6db973fc84935cabdddcd036d86dd3cbd
davenfroberg <davenfroberg@gmail.com>
2022-08-19 12:27:43 -0600

    Implemented functionality for buttons

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Node {
 }
 
 let dim = 10;
-let targPos, startPos;
+let targX, targY, startX, startY;
 let wallProbability = 30; // in percent
 let map = [];
 
@@ -38,43 +38,99 @@ function assignWalls() {
     }
 }
 
-function assignTerminus(terminus) {
+function randomAssignTerminus(terminus) {
     let randomX = 0;
     let randomY = 0;
     do {
         randomX = Math.floor(Math.random() * dim);
         randomY = Math.floor(Math.random() * dim);
-    } while (!map[randomX][randomY].wall);
+    } while (map[randomX][randomY].wall);
     map[randomX][randomY][terminus] = true;
+    return (randomY * dim + randomX);
 }
-
 
 function createMap() {
     for (let i = 0; i < dim; i++) {
         map[i] = createRow(i);
     }
     assignWalls();
-    assignTerminus('isStart');
-    assignTerminus('isTarget');
+
+    let startPos = randomAssignTerminus('isStart');
+    let targPos = randomAssignTerminus('isTarget');
+
+    startX = fromPosToX(startPos);
+    startY = fromPosToY(startPos);
+    targX = fromPosToX(targPos);
+    targY = fromPosToY(targPos);
 }
 
+function fromPosToX(pos) {
+    return pos - Math.floor(pos/dim) * dim;
+}
+
+function fromPosToY(pos) {
+    return Math.floor(pos/dim);
+}
 
 // -------------------- JavaScript for GUI -----------------------------------
 
 let slider = document.getElementById("slider");
 let output = document.getElementById("dimension");
+let assigningStart = false;
+let assigningTarget = false;
 
+function clickStart(startButton, targetButton) {
+    assigningTarget = false;
+    targetButton.className = 'unselected';
+    if (!assigningStart) {
+        assigningStart = true;
+        startButton.className = 'selected';
+    }
+    else {
+        assigningStart = false;
+        startButton.className = 'unselected';
+    }
+}
 
-function toggleWall(el, r, c) {
+function clickTarget(targetButton, startButton) {
+    assigningStart = false;
+    startButton.className = 'unselected';
+
+    if (!assigningTarget) {
+        assigningTarget = true;
+        targetButton.className = 'selected';
+    }
+    else {
+        assigningTarget = false;
+        targetButton.className = 'unselected';
+    }
+}
+
+function clickCell(el, r, c) {
     const node = map[c][r];
     if (!node.isStart && !node.isTarget) {
-
-        if (node.wall) {
-            node.wall = false;
+        if (assigningStart) {
+            if (!node.wall) {
+                node.isStart = true;
+                map[startX][startY].isStart = false;
+                startX = c;
+                startY = r;
+            }
+        } else if (assigningTarget) {
+            if (!node.wall) {
+                node.isTarget = true;
+                map[targX][targY].isTarget = false;
+                targX = c;
+                targY = r;
+            }
         } else {
-            node.wall = true;
+            console.log('click!');
+            if (node.wall) {
+                node.wall = false;
+            } else {
+                node.wall = true;
+            }
         }
-
         repaint();
     }
 }
@@ -82,12 +138,12 @@ function toggleWall(el, r, c) {
 
 function regenerateMap() {
     document.body.removeChild(grid);
-    grid = createGrid(dim, dim, toggleWall, true);
+    grid = createGrid(dim, dim, clickCell, true);
     document.body.appendChild(grid);
 }
 function repaint() {
     document.body.removeChild(grid);
-    grid = createGrid(dim, dim, toggleWall, false);
+    grid = createGrid(dim, dim, clickCell, false);
     document.body.appendChild(grid);
 }
 
@@ -95,6 +151,19 @@ slider.oninput = function () {
     dim = this.value;
     output.innerHTML = dim + ' x ' + dim + ' grid';
     regenerateMap();
+}
+
+function handleButtons() {
+    let startButton = document.getElementById('start-button');
+    let targetButton = document.getElementById('target-button');
+
+    startButton.addEventListener('click', function() {
+        clickStart(startButton, targetButton);
+      });
+
+    targetButton.addEventListener('click', function() {
+        clickTarget(targetButton, startButton);
+      });
 }
 
 function createGrid(rows, cols, callback, regen) {
@@ -127,5 +196,7 @@ function createGrid(rows, cols, callback, regen) {
     return table;
 }
 
-let grid = createGrid(dim, dim, toggleWall, true);
+handleButtons();
+
+let grid = createGrid(dim, dim, clickCell, true);
 document.body.appendChild(grid);
